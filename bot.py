b3570967becd01d7e587564132a2ea40b00107e6
Swingsensey <swingsniperbot@gmail.com>
2026-04-20 19:26:12 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -27,7 +27,7 @@ SESSION_STR = os.getenv('TELEGRAM_SESSION')
 SJ_KEY = os.getenv('SUPERJOB_KEY') # Твой новый ключ
 # Список каналов БЕЗ собаки @
 CHANNELS = [
-    'vdhl_good', 'mediajobs_ru', 'work_in_media', 'moviestart_ru', 'distantsiya',
+    'vdhl_good', 'mediajobs_ru', 'work_in_media', 'distantsiya',
     'theblueprintcareer', 'huggabletalents', 'careerspace', 'morejobs', 'heyanie', 
     'marketing_jobs', 'mirkreatorovjob', 'budujobs', 'normrabota', 'jobpower', 
     'it_vakansii_jobs', 'workasap', 'forproducer', 'vacanciesrus', 
@@ -52,11 +52,19 @@ dp = Dispatcher(bot)
 logging.basicConfig(level=logging.INFO)
 
 HEADERS = {
-    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36',
-    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8',
-    'Accept-Language': 'ru-RU,ru;q=0.9',
-    'Referer': 'https://www.google.com/',
-    'Connection': 'keep-alive'
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36',
+    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
+    'Accept-Language': 'ru-RU,ru;q=0.9,en-US;q=0.8,en;q=0.7',
+    'Cache-Control': 'no-cache',
+    'Pragma': 'no-cache',
+    'Sec-Ch-Ua': '"Chromium";v="124", "Google Chrome";v="124", "Not-A.Brand";v="99"',
+    'Sec-Ch-Ua-Mobile': '?0',
+    'Sec-Ch-Ua-Platform': '"Windows"',
+    'Sec-Fetch-Dest': 'document',
+    'Sec-Fetch-Mode': 'navigate',
+    'Sec-Fetch-Site': 'none',
+    'Sec-Fetch-User': '?1',
+    'Upgrade-Insecure-Requests': '1'
 }
 
 # --- БАЗА ДАННЫХ ---
